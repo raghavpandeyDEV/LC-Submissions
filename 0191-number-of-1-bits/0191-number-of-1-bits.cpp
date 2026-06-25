dea6cd3b1e9c3f12c8867add783c21bbbbaa7005
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-06-25 21:10:26 +0530

    Time: 0 ms (100%), Space: 8.1 MB (81.74%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-     string decToBinary(int n) {
-        string res="";
-        while(n!=1){
-           res+=(n%2)+'0';
-           n=n/2;
-        }
-        res+=(n)+'0';
-        reverse(res.begin(),res.end());
-        return res;
-    }
-
     int hammingWeight(int n) {
-        string ans=decToBinary(n);
         int cnt=0;
-        for(int i=0;i<ans.size();i++){
-          if(ans[i]=='1')cnt++;
+        while(n>0){
+        cnt+=n%2;
+        n=n/2;
         }
-
-    return cnt;
+        
+        return cnt;
 
     }
 };
