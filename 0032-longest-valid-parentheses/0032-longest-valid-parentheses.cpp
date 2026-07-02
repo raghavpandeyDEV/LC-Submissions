9b96fcddaa393ef14714996367987a22b9c73e40
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-03 01:43:54 +0530

    Time: 0 ms (100%), Space: 10.3 MB (94.9%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int>st;
-        vector<int>vis(s.size(),0);
+        int maxi=0;
+        int open=0;
+        int close=0;
+        int n=s.size();
 
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(')st.push(i);
-            else{
-                if(!st.empty()){
-                    int matching_el=st.top();
-                    st.pop();
-                    vis[i]=1;
-                    vis[matching_el]=1;
-                }
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')open++;
+            if(s[i]==')')close++;
+            if(close==open)maxi=max(maxi,close+open);
+
+           else if(close>open){
+                open=0;
+                close=0;
             }
         }
-        int maxi =0 , cnt =0;
+
+        open=0;
+        close=0;
+
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='(')open++;
+            if(s[i]==')')close++;
+            if(close==open)maxi=max(maxi,close+open);
+
+           else if(open>close){
+                open=0;
+                close=0;
+            }
+        } 
         
-        for(int i=0;i<vis.size();i++){
-            if(vis[i]==1){
-                cnt++;
-                maxi=max(maxi,cnt);
-            }
-            else{
-                cnt=0;
-            }
-        }
         return maxi;
     }
 };
