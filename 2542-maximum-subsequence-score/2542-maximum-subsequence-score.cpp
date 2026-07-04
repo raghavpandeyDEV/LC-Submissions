b21f83a7b5814683d88c52dbb1b2bb2bdc8ae67b
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-05 01:20:52 +0530

    Time: 36 ms (96.94%), Space: 95.8 MB (59.59%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
-            vector<pair<int,int>> v;
 
-        for (int i = 0; i < nums1.size(); i++) {
-          v.push_back({nums1[i], nums2[i]});
-}
+        // use PQ for nums1 
+        // sort desc order nums2
+
+        vector<pair<int,int>>v;
+        for(int i=0;i<nums1.size();i++){
+            v.push_back({nums1[i],nums2[i]});
+        }
 
         sort(v.begin(), v.end(), [](auto &p1, auto &p2) {
     return p1.second > p2.second;
@@ -13,28 +16,30 @@ public:
         
            priority_queue<int,vector<int>,greater<int>> pq;
 
+           long long sum=0;
            int min_el=v[k-1].second;
-           long long ksum=0;
+
            for(int i=0;i<k;i++){
-            ksum+=v[i].first;
+            sum+=v[i].first;
             pq.push(v[i].first);
            }
 
-        
-        long long score=ksum*min_el;
-        for(int i=k;i<nums1.size();i++){
+           long long score=sum*min_el;
+
+           for(int i=k;i<nums1.size();i++){
             min_el=v[i].second;
-
-             ksum += v[i].first;
-            ksum -= pq.top();
+            sum+=(v[i].first);
+            sum-=(pq.top());
             pq.pop();
-             pq.push(v[i].first);
 
-
-        
-        score=max(score,1LL * min_el * ksum);
+            pq.push(v[i].first);
             
-        }
-           return score;
+            score=max(score,sum*min_el);
+           }
+
+
+
+return score;
+        
     }
 };
