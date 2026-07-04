e13b1f29211989a49ab14f80976f46d95ede6b22
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-05 01:50:06 +0530

    Time: 51 ms (15.61%), Space: 48.7 MB (11.91%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -3,42 +3,41 @@ public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int m=maze.size();
         int n=maze[0].size();
-        vector<vector<int>>dis(m,vector<int>(n,1e9));
+        vector<vector<int>>vis(m,vector<int>(n,-1));
 
-        priority_queue<
-    pair<int, pair<int,int>>,
-    vector<pair<int, pair<int,int>>>,
-    greater<pair<int, pair<int,int>>>
-> pq;
- 
-     int sr=entrance[0];
-     int sc=entrance[1];
-     pq.push({0,{sr,sc}});
-     dis[sr][sc]=0;
+        int sr=entrance[0];
+        int sc=entrance[1];
 
-     while(!pq.empty()){
-        int d=pq.top().first;
-        int r=pq.top().second.first;
-        int c=pq.top().second.second;
-       if(!(r == sr && c == sc) &&
-   (r == 0 || r == m-1 || c == 0 || c == n-1))
-    return d;
+        vis[sr][sc]=1;
+        queue<pair<pair<int,int>,int>>q;  //{{r,c},d};
+      q.push({{sr,sc},0});
+      
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int d=q.front().second;
 
-       pq.pop();
 
-       vector<int>drow={1,0,-1,0};
-       vector<int>dcol={0,1,0,-1};
+            q.pop();
 
-       for(int i=0;i<4;i++){
-        int nrow=r+drow[i];
-        int ncol=c+dcol[i];
-if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && maze[nrow][ncol]=='.' && dis[nrow][ncol]>d+1){
-    pq.push({d+1,{nrow,ncol}});
-    dis[nrow][ncol]=d+1;
-}
-       }
-     }
-    return -1;
+           if ((r != sr || c != sc) &&
+                (r == 0 || r == m - 1 || c == 0 || c == n - 1))
+                return d;
+        
 
+        vector<int>drow={1,0,-1,0};
+        vector<int>dcol={0,-1,0,1};
+
+        for(int i=0;i<4;i++){
+            int nrow=r+drow[i];
+            int ncol=c+dcol[i];
+
+    if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && maze[nrow][ncol]=='.' && vis[nrow][ncol]==-1){
+        q.push({{nrow,ncol},d+1});
+        vis[nrow][ncol]=1;
+    }
+        }
+        }
+        return -1;
     }
 };
