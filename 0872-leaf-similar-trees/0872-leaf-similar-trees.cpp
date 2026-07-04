4bcc48ea1c1a25110c8d65e00f71c0b926da50cf
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-05 01:31:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -10,35 +10,32 @@
  * };
  */
 class Solution {
-public:
-   bool isLeaf(TreeNode* node){
-     
-    if(node->left ==NULL && node->right==NULL)return true;
+public: 
+   bool isLeaf(TreeNode*root){
+    if(root->left==NULL && root->right==NULL)return true;
     return false;
    }
-   
-   void traverse(TreeNode*root , vector<int>&ans){
-    if(root == NULL) return;
-    if(isLeaf(root)){
-        ans.push_back(root->val);
-        return ;
-    }
-    traverse(root->left,ans);
-    traverse(root->right,ans);
-   }
 
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>arr1;
-        traverse(root1,arr1);
-        vector<int>arr2;
-        traverse(root2,arr2); 
+    void traverse(TreeNode*root , vector<int>&ans){
+        if(!root)return;
 
-        if(arr1.size()!=arr2.size())return false;
-
-        for(int i=0;i<arr1.size();i++){
-            if(arr1[i]!=arr2[i])return false;
+        if(isLeaf(root)){
+            ans.push_back(root->val);
+            return;
         }
-        return true;
+        traverse(root->left, ans);
+        traverse(root->right,ans);
+        
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int>nums1 ;
+        vector<int>nums2 ;
+        traverse(root1,nums1);
+        traverse(root2,nums2);
+        for(int i=0;i<nums1.size();i++){
+            if(nums1[i]!=nums2[i])return false;
+        }
 
+  return true;
     }
 };
