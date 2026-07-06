012af3151632cdd95242794bad77bac3af55ae25
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-06 17:26:53 +0530

    Time: 2 ms (65.12%), Space: 9.8 MB (32.76%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool solve(int i , int j , int k , string&s1, string&s2, string&s3,    vector<vector<vector<int>>>&dp){
+    bool solve(int i , int j , string&s1, string&s2, string&s3,vector<vector<int>>&dp){
           
-      int n=s1.size();
+        int n=s1.size();
         int m=s2.size();
         int N=m+n;
 
-        if(i==n && j==m && k==N)return true;
-        if(k>=N)return false;//other string doesnt get consumed at all
-         if(dp[i][j][k]!=-1)return dp[i][j][k];
+        if(i==n && j==m && i+j==N)return true;
+        if(i+j>=N)return false;//other string doesnt get consumed at all
+        if(dp[i][j]!=-1)return dp[i][j];
         bool result1=false;
         bool result2=false;
 
-        if(s1[i]==s3[k]){
-           result1= solve(i+1,j,k+1,s1,s2,s3,dp);
+        if(s1[i]==s3[i+j]){
+           result1= solve(i+1,j,s1,s2,s3,dp);
         }
-        if(s2[j]==s3[k]){
-        result2=solve(i,j+1,k+1,s1,s2,s3,dp);
+        if(s2[j]==s3[i+j]){
+        result2=solve(i,j+1,s1,s2,s3,dp);
         }
-        return dp[i][j][k]= result1 || result2;
+        return dp[i][j]= result1 || result2;
       
     }
     bool isInterleave(string s1, string s2, string s3) {
@@ -27,7 +27,7 @@ public:
         int N=m+n;
 
         if(s3.size()!=N)return false;
-    vector<vector<vector<int>>> dp(n + 1,vector<vector<int>>(m + 1, vector<int>(N + 1, -1)));
-        return solve(0,0,0,s1,s2,s3,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solve(0,0,s1,s2,s3,dp);
     }
 };
