3120f24110e7935c3aff1823c6ca0e8b4ff73927
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-20 15:23:22 +0530

    Time: 69 ms (55.31%), Space: 14.2 MB (77.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int solve(int i ,int prv , vector<int>& nums , vector<vector<int>>&dp ){
-        if(i==nums.size()){
-            return 0;
-        }
-        if(dp[i][prv+1]!=-1)return dp[i][prv+1];
-        int notPick=solve(i+1,prv,nums,dp);
-        int pick=0;
-        if(prv==-1 || nums[i]>nums[prv])pick=1+solve(i+1,i,nums,dp);
-
-        return dp[i][prv+1]= max(pick,notPick);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-    
-        for(int i=n-1;i>=0;i--){
-            for(int prv=i-1;prv>=-1;prv--){
-        int notPick=dp[i+1][prv+1];
-        int pick=0;
-        if(prv==-1 || nums[i]>nums[prv])pick=1+dp[i+1][i+1];
+      // special code 
+      int n=nums.size();
 
-         dp[i][prv+1]= max(pick,notPick);
+      vector<int>dp(n,1);
+
+      for(int idx=0;idx<n;idx++){
+        for(int prv=0;prv<idx;prv++){
+            if(nums[prv]<nums[idx]){
+                dp[idx]=max(dp[idx],1+dp[prv]);
             }
         }
-
-return dp[0][-1+1];
-        //second state me har jgh + 1
+      }
+      int maxi=INT_MIN;
+      
+      for(int i=0;i<n;i++){
+        maxi=max(maxi,dp[i]);
+      }
+      return maxi;
     }
 };
