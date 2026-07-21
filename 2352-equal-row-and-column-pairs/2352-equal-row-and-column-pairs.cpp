0e0a0699f72491bcffff250023de1f65c3d6b5dc
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-21 19:03:44 +0530

    Time: 20 ms (48.13%), Space: 37 MB (52.81%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+        map<vector<int>, int> freq;
         int n=grid.size();
-        map<vector<int>,vector<int>>rows;
-        map<vector<int>,vector<int>>columns;
 
-        for(int i=0;i<n;i++){
-            vector<int>row;
-            for(int j=0;j<n;j++){
-               row.push_back(grid[i][j]);
-            }
-            rows[row].push_back(i);
+        for (auto& row : grid)
+            freq[row]++;
+
+        int cnt = 0;
+
+        for (int j = 0; j < n; j++) {
+            vector<int> col;
+            for (int i = 0; i < n; i++)
+                col.push_back(grid[i][j]);
+
+            cnt += freq[col];
         }
-
-        for(int j=0;j<n;j++){
-            vector<int>column;
-            for(int i=0;i<n;i++){
-               column.push_back(grid[i][j]);
-            }
-            columns[column].push_back(j);
-        }
-         
-        int cnt=0;
-
-       for(auto it : rows){
-        if(columns.find(it.first)!=columns.end()){
-            cnt += it.second.size() * columns[it.first].size();
-        }
-       }
-     return cnt;
-
+        return cnt;
     }
 };
