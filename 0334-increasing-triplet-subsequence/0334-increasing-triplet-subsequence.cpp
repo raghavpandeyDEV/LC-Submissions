12ba7d5be11a3a6bae91dab615f05a12b5c8cca6
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-21 13:43:37 +0530

    Time: 3 ms (39.65%), Space: 115.6 MB (92.19%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -16,4 +16,44 @@ public:
         
         return false;
     }
+
+    //Q) [ 1  , 4 , 0 , 6 ] -> num1= 0 , num2=4 , num3=6 (0,4,6) but actual is (1,4,6)
+
+ /*   The key insight
+
+num1 and num2 are not necessarily the actual triplet.
+
+They only store values, not the exact indices of the final answer.
+
+When we reached 4, we already had
+
+1 ----> 4
+
+which is a perfectly valid increasing pair.
+
+Later, when 0 arrives, we simply say
+
+"If I ever want to start a new increasing sequence, 0 is an even better first element."
+
+We are not destroying the fact that (1,4) already existed.
+
+So after reading 0, internally we have two possibilities:
+
+Existing pair:
+1 -> 4
+
+Potential new start:
+0
+
+Then 6 arrives.
+
+Since 6 > 4, we know there already existed some first element before 4.
+
+That first element is 1, not necessarily the current num1.
+
+Hence the actual triplet is
+
+1 -> 4 -> 6
+*/
+
 };
