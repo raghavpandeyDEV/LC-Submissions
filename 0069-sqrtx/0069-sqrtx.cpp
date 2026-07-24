54de8020c96e801f2b24ba868aa373d9c7924cb0
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-25 00:15:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87.71%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        int ans=-1;
+
+        while(low<=high){
+            long long mid=(low+high)/2;
+
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid<x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
