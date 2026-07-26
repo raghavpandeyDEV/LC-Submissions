ebb794fa77887853d6d734c40b8a7128d08790a9
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-26 15:05:45 +0530

    Time: 18 ms (49.33%), Space: 26.2 MB (38.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,15 +3,15 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
         unordered_map<string,vector<string>>mpp;
-
-        for(int i=0;i<strs.size();i++){
-            string word=strs[i];
-            sort(strs[i].begin(),strs[i].end());
-            mpp[strs[i]].push_back(word);
+        
+        for(auto it : strs){
+            string org=it;
+            sort(it.begin(),it.end());
+            mpp[it].push_back(org);
         }
 
         for(auto it : mpp){
-            ans.push_back(it.second);
+          ans.push_back(it.second);  
         }
         return ans;
     }
