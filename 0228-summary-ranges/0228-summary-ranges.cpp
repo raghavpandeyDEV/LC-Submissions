08b50633b753b2ad1179606ed957638fc6572f44
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-26 16:40:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+     vector<string>ans;
+     int i=0;
+      int n=nums.size();
+
+      while(i<n){
+        int start=nums[i];
+        while(i<n && nums[i]==nums[i+1]-1){
+           i++;
+        }
+        int end=nums[i];
+        if(start!=end){
+        ans.push_back(to_string(start) + "->" + to_string(end));
+        }
+        else{
+            ans.push_back(to_string(nums[i]));
+        }
+       i++;
+      }   
+      return ans;
+      
+    }
+};
