2108363865671204402a8308de0839b5dbf4af70
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-26 14:35:33 +0530

    Time: 2 ms (58.5%), Space: 11.8 MB (75.16%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int>hash(26,0);
+        for(int i=0;i<ransomNote.size();i++){
+            hash[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<magazine.size();i++){
+            if(hash[magazine[i]-'a']== 0)continue;
+            else hash[magazine[i]-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(hash[i]!=0)return false;
+        }
+        return true;
+    }
+};
