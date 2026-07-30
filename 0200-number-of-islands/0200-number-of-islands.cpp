183306b650607049ef96246c385c5f6b5272c920
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-30 21:16:43 +0530

    Time: 53 ms (7.9%), Space: 26.2 MB (6.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
-    void bfs(int r , int c , vector<vector<int>>&vis , vector<vector<char>>& grid){
-         int m=grid.size();
-        int n=grid[0].size();
-
-        queue<pair<int,int>>q;
-        q.push({r,c});
+    void bfs(vector<vector<int>>&vis ,vector<vector<char>>& grid,queue<pair<int,int>>&q){
+           int m=grid.size();
+           int n=grid[0].size();
 
         while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
 
+            int i=q.front().first;
+            int j=q.front().second;
             q.pop();
 
-            vector<int>drow={1,0,-1,0};
-            vector<int>dcol={0,-1,0,1};
-
-    for(int i=0;i<4;i++){
-        int nrow=r+drow[i];
-        int ncol=c+dcol[i];
-
- if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && grid[nrow][ncol]=='1' && vis[nrow][ncol]==-1){
-  q.push({nrow,ncol});
-  vis[nrow][ncol]=1;
- }
+        vector<int>drow={-1,0,1,0};
+        vector<int>dcol={0,1,0,-1};
+        
+        for(int idx=0;idx<4;idx++){
+            int nrow=i+drow[idx];
+            int ncol=j+dcol[idx];
+    if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && vis[nrow][ncol]==-1 && grid[nrow][ncol]=='1'){
+        vis[nrow][ncol]=1;
+        q.push({nrow,ncol});
     }
         }
+
+    }
     }
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+        
+ int m=grid.size();
+         int n=grid[0].size();
+         vector<vector<int>>vis(m , vector<int>(n,-1));
+         int cnt=0;
+          
+         queue<pair<int,int>>q;
 
-        vector<vector<int>>vis(m,vector<int>(n,-1));
-       int cnt=0;
-
-        for(int i=0;i<m;i++){
+         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-               if(grid[i][j]=='1' && vis[i][j]==-1 ){
-                bfs(i,j,vis,grid);
-                cnt++;
-               }
+                if(grid[i][j]=='1' && vis[i][j]==-1){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    bfs(vis,grid,q);
+                    cnt++;
+                }
             }
-        }
-        return cnt;
+         }
+         return cnt;
     }
 };
