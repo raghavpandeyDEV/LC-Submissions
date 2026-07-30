72ee3d27e8e562fcac14914da7b9986f28d20220
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-07-30 22:02:45 +0530

    Time: 3 ms (26.06%), Space: 17.7 MB (7.22%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,47 +3,48 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int cntFresh=0;
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        queue<pair<pair<int,int>,int>>q;   // {{r,c},t}
+
+        int fresh=0;
+        vector<vector<int>>vis(m,vector<int>(n,-1));
+        queue<pair<pair<int,int>,int>>q; // {{r,c},t}
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                if(grid[i][j]==1)fresh++;
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                     vis[i][j]=1;
                 }
-                if(grid[i][j]==1){
-                    cntFresh++;
-                }
             }
         }
-
-         int drow[4]={-1,0,1,0};
-          int dcol[4]={0,1,0,-1};
+        
         int tm=0;
         int cnt=0;
-
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
             int t=q.front().second;
-            tm=max(t,tm);
+
+            tm=max(tm,t);
+
             q.pop();
 
+            vector<int> drow = {-1, 0, 1, 0};
+vector<int> dcol = {0, 1, 0, -1};
+
             for(int i=0;i<4;i++){
-                int nrow=drow[i]+r;
-                int ncol=dcol[i]+c;
-     if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-        vis[nrow][ncol]=1;
-        q.push({{nrow,ncol},t+1});
-        cnt++;
-     }
-
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && vis[nrow][ncol]==-1 && grid[nrow][ncol]==1){
+    vis[nrow][ncol]=1;
+    q.push({{nrow,ncol},t+1});
+    cnt++;
+}
             }
-        }
-      if(cntFresh!=cnt) return -1;
-      return tm;
 
+        }
+        if(cnt==fresh)return tm;
+        return -1;
+        
     }
 };
