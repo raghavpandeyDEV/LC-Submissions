f5771ca4b03ebec14d8eb66687a2c6f26e21d9e1
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-07 03:47:46 +0530

    Time: 3 ms (23.41%), Space: 8.9 MB (39.73%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        int n=s.size();
-       
-        vector<int>ans;
-
         unordered_map<char,int>mpp;
-        for(int i=0;i<n;i++){
+        vector<int>ans;
+        for(int i=0;i<s.size();i++){
             mpp[s[i]]=i;
         }
 
-         int i=0;
-        while(i<n){
+        int i=0;
+        while(i<s.size()){
             int start=i;
-            int maxRange=mpp[s[i]];
-            while(i<=maxRange){
-                maxRange=max(maxRange,mpp[s[i]]);
+            int end=mpp[s[i]];
+            while(i<=end){
+                end=max(end,mpp[s[i]]);
                 i++;
             }
-            ans.push_back(maxRange-start+1);
-
+            ans.push_back(end-start+1);
+            
         }
         return ans;
     }
