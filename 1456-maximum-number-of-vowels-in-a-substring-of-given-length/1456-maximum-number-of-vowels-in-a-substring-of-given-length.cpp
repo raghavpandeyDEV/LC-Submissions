05e4874ffc726756b19470c573922af47cc91663
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-07 03:20:03 +0530

    Time: 0 ms (100%), Space: 13.3 MB (28.47%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public: 
+public:
     bool isVowel(char ch){
         if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')return true;
         return false;
@@ -12,23 +12,22 @@ public:
         int l=0;
         int r=k-1;
 
-        for(int i=0;i<k;i++){
+        // initial window
+        for(int i=l;i<=r;i++){
             if(isVowel(s[i]))cnt++;
+        }
+        maxi=cnt;
+
+        while(r<n-1){
+            r++;
+            if(isVowel(s[r]))cnt++;
+            
+            if(isVowel(s[l]))cnt--;
+            l++;
+
             maxi=max(maxi,cnt);
         }
-    
-        while(r < n-1){
-        // r ko move krne k baad check kro ki naya wala vowel h kya
-         r++;
-         if(isVowel(s[r]))cnt++;
-         // l ko move krne k phle check kro vowel h ya ni
-         if(isVowel(s[l]))cnt--;
-         l++;
-        
-         maxi=max(maxi,cnt);
 
-        }
-
-       return maxi;
+        return maxi;
     }
 };
