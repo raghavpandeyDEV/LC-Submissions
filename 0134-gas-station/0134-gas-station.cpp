d4b195b0ee8d38961a90b90fd37bc3bf50a3d927
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-07 03:13:12 +0530

    Time: 0 ms (100%), Space: 112.5 MB (26.67%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -4,23 +4,30 @@ public:
         int totalGas=0;
         int totalCost=0;
 
-        for(int i=0;i<gas.size();i++){
+        int n=cost.size();
+
+        for(int i=0;i<n;i++){
             totalGas+=gas[i];
             totalCost+=cost[i];
         }
-
         if(totalCost>totalGas)return -1;
-         int start=0;
-         int currGas=0;
-        for(int i=0;i<gas.size();i++){
-           currGas +=(gas[i]-cost[i]);
 
-            if(currGas<0){
-                start=i+1;
-                currGas=0;
-                continue;
+        int currGas=0;
+        int currCost=0;
+        int start=0;
+
+        for(int i=0;i<n;i++){
+
+            currGas+=gas[i];
+            currCost+=cost[i];
+            if(currCost>currGas){
+               start=i+1;
+               currGas=0;
+               currCost=0;
+               continue;
             }
         }
-        return start;
+
+       return start;
     }
 };
