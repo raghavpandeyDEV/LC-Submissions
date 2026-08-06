477a6e9f5499dfeef943a0fb849d621df4b6dd6e
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-07 03:37:01 +0530

    Time: 107 ms (22.41%), Space: 88.3 MB (64.56%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,19 +11,16 @@
  */
 class Solution {
 public:
-    void solve(TreeNode*root , int maxi , int&cnt){
-        if(root==NULL)return ;
-
+    void solve(TreeNode* root , int maxi , int&cnt){
+        if(!root)return ;
         if(root->val>=maxi)cnt++;
-          solve(root->left , max(maxi,root->val) , cnt);
-          solve(root->right , max(maxi,root->val) , cnt);
-        
 
+        solve(root->left , max(maxi,root->val) , cnt);
+        solve(root->right, max(maxi,root->val) , cnt);
     }
     int goodNodes(TreeNode* root) {
         int cnt=0;
         solve(root,root->val,cnt);
         return cnt;
-
     }
 };
