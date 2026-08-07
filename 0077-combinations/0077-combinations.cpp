fee0515a1cd2377659dd3e1db02856566bd1796b
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-07 16:19:45 +0530

    Time: 42 ms (62.38%), Space: 62.7 MB (63.32%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-   void solve(int i , int k , vector<int>&nums,vector<int>&temp , vector<vector<int>>&ans){
-   
-    if(temp.size()==k){
-        ans.push_back(temp);
-        return;
-    }   
-    if(i>=nums.size())return;
-   
-   
-    
-    temp.push_back(nums[i]);
-    solve(i+1,k,nums,temp,ans);
-    temp.pop_back();
-    solve(i+1,k,nums,temp,ans);
-
-   }
-    vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>>ans;
-        vector<int>nums;
-        vector<int>temp;
-        for(int i=1;i<=n;i++){
-            nums.push_back(i);
+    void solve(int i , int k , vector<int>&arr,vector<int>&temp , vector<vector<int>>&ans){
+        if(temp.size()==k){
+            ans.push_back(temp);
+            return;
         }
-        solve(0,k,nums,temp,ans);
-        return ans;
+
+        if(i==arr.size())return;
+
+        temp.push_back(arr[i]);
+        solve(i+1,k,arr,temp,ans);
+        temp.pop_back();
+        solve(i+1,k,arr,temp,ans);
+
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+       vector<int>arr;
+       vector<int>temp;
+       vector<vector<int>>ans;
+       for(int i=1;i<=n;i++){
+        arr.push_back(i);
+       } 
+
+       solve(0,k,arr,temp,ans);
+       return ans;
     }
 };
