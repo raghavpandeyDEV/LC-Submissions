102813730205383c532a3e95e3a93f9f1e4a46e3
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-09 22:56:13 +0530

    Time: 183 ms (93.27%), Space: 10.5 MB (81.45%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool found(int i , int j , vector<vector<char>>& board , int k, string&word ){
+    bool solve(int i , int j , vector<vector<char>>& board , int idx ,string&word ){
         int m=board.size();
         int n=board[0].size();
+        if(idx==word.size())return true;
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!=word[idx])return false;
 
-        if(k==word.size())return true;
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!=word[k])return false;
-
-        char temp=board[i][j];
+        char ch=board[i][j];
         board[i][j]='#';
 
-        bool up=found(i-1,j,board,k+1,word);
-        bool down=found(i+1,j,board,k+1,word);
-        bool left=found(i,j-1,board,k+1,word);
-        bool right=found(i,j+1,board,k+1,word);
+        bool up = solve(i-1,j,board,idx+1,word);
+        bool down= solve(i+1,j,board,idx+1,word);
+        bool left = solve(i,j-1,board,idx+1,word);
+        bool right= solve(i,j+1,board,idx+1,word);
 
-        board[i][j]=temp;
-        return up || down || left || right;
+        board[i][j]=ch;
+        return (up || down || left || right);
+
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m=board.size();
         int n=board[0].size();
-
+       
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    if(found(i,j,board,0,word))return true;
+                    if(solve(i,j,board,0,word)==true)return true;
                 }
             }
         }
-        return false;
+     return false;
     }
 };
