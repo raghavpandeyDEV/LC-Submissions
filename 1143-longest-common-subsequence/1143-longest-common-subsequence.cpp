faa546362b25a991b15f3a1886a31ff93f1e8b89
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-26 20:37:17 +0530

    Time: 46 ms (15.82%), Space: 27.5 MB (55.69%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,7 +6,7 @@ public:
 
         if(dp[i][j]!=-1)return dp[i][j];
 
-        if(s1[i]==s2[j])return 1 + solve(i-1, j-1 ,s1,s2,dp);
+        if(s1[i]==s2[j])return dp[i][j]= 1 + solve(i-1, j-1 ,s1,s2,dp);
 
         return dp[i][j]=max(solve(i,j-1,s1,s2,dp),solve(i-1,j,s1,s2,dp));
     }
