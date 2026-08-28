6dde0c8287bd8f04521b180ec1bd18a2bbd391d9
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-08-28 22:48:17 +0530

    Time: 7 ms (61.07%), Space: 18.4 MB (28.57%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -18,12 +18,41 @@ public:
         int n=prices.size();
 
        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(2, vector<int>(k+1, -1))
+            n+1, vector<vector<int>>(2, vector<int>(k+1,0))
         );
+       
+       
+       //cnt==0;
+       for(int i=0;i<n;i++){
+        for(int buy=0;buy<=1;buy++){
+            dp[i][buy][0]=0;
+        }
+       }
 
-        return solve(0,1,prices,k,dp);
+       // i==n
+       for(int buy=0;buy<=1;buy++){
+        for(int cnt=0;cnt<=k;cnt++){
+            dp[n][buy][cnt]=0;
+        }
+       }
 
-        //tc: O ( n )
-        //sc:o(n)+O(n)
+       for(int i=n-1;i>=0;i--){
+        for(int buy=1;buy>=0;buy--){
+            for(int cnt=k;cnt>0;cnt--){
+            
+             int profit=0;
+
+        if(buy){
+            profit=max(-prices[i]+dp[i+1][0][cnt] , dp[i+1][1][cnt]);
+        }
+        else{
+            profit=max(prices[i]+dp[i+1][1][cnt-1], dp[i+1][0][cnt]);
+        }
+         dp[i][buy][cnt]=profit;
+
+            }
+        }
+       }
+        return dp[0][1][k];
     }
 };
