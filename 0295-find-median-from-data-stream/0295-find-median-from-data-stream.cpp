8a055008f81f34321428e1ca8febb28fe2564416
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-09-04 23:32:48 +0530

    Time: 67 ms (69.3%), Space: 148.7 MB (30.1%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,29 +1,40 @@
 class MedianFinder {
-    priority_queue<int>left_max;
-    priority_queue<int,vector<int>,greater<int>>right_min;
 public:
+   // left -> max heap
+   // right -> min heap
+
+   priority_queue<int>left_max;
+   priority_queue<int , vector<int>  , greater<int>>right_min;
+
+
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(left_max.size()==0 || num<=left_max.top())left_max.push(num);
+        // add to left max heap
+        if(left_max.size()==0 || num <=left_max.top())left_max.push(num);
         else right_min.push(num);
 
-        if((left_max.size() > right_min.size() + 1)){
-            right_min.push(left_max.top());
-            left_max.pop();
-        }
-        else if(right_min.size()>left_max.size()){
+      // left size greater than right size by 2 or more
+       if(left_max.size() > right_min.size() + 1){
+          right_min.push(left_max.top());
+          left_max.pop();
+       }
+      
+       // if right size is greater than left by 1
+        if(left_max.size() +1  == right_min.size()){
             left_max.push(right_min.top());
             right_min.pop();
         }
+    
+    
+
     }
     
     double findMedian() {
-        if(left_max.size()==right_min.size()){
-            double mean = ((left_max.top()+right_min.top()))/2.0;
-            return mean;
+        if(right_min.size()== left_max.size()){
+            return (left_max.top()+right_min.top())/2.0;
         }
         return left_max.top();
     }
