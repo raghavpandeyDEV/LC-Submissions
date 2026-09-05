0d17ec2496e6e93dd652ce07663eb73eb845fbeb
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-09-05 15:52:56 +0530

    Time: 12 ms (36.36%), Space: 18.4 MB (28.32%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -18,11 +18,41 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
         // 3d matrix -> n * 2 * cnt+1
+        // memo -> top down 
+        // tabulation -> bottom up approach
 
-        vector<vector<vector<int>>>dp(n , vector<vector<int>>(2 , vector<int>(k+1,-1)));
-        return solve(0,1,k,prices,dp);
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2 , vector<int>(k+1,0)));
+        
+        // i==n
+        for(int buy=0;buy<2;buy++){
+            for(int cnt=0;cnt<=k;cnt++){
+                dp[n][buy][cnt]=0;
+            }
+        }
 
-        // TC : O(2^n)
-        //SC : O(n)
+        // cnt==0
+        for(int i=0;i<n;i++){
+            for(int buy=0;buy<2;buy++){
+                dp[i][buy][0]=0;
+            }
+        }
+       
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                for(int cnt=1;cnt<=k;cnt++){
+                    int profit=0;
+    if(buy){
+        profit=max(-prices[i]+dp[i+1][0][cnt], dp[i+1][1][cnt]);
+    }
+    else{
+        profit=max(prices[i]+dp[i+1][1][cnt-1], dp[i+1][0][cnt]);
+    }
+    dp[i][buy][cnt]= profit;
+                }
+            }
+        }
+    return dp[0][1][k];
+        // TC : O(n*k)
+        //SC : O(n*k)+O(n)
     }
 };
