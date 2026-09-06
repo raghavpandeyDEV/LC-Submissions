d730317d736e6bef5ed4d0645a0051a971d567f1
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-09-06 09:24:26 +0530

    Time: 0 ms (100%), Space: 27.2 MB (32.99%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    vector<int>leftMax(vector<int>&arr){ // max height to the left of the index
-       vector<int>ans(arr.size());
-       ans[0]=0;
-       for(int i=1;i<arr.size();i++){
-        ans[i]=max(ans[i-1] , arr[i-1]);
-       }
-       return ans;
-    }
 
-     vector<int>rightMax(vector<int>&arr){ // max height to the right of the index
-       int n=arr.size();
-       vector<int>ans(arr.size());
-       ans[n-1]=0;
-       for(int i=n-2;i>=0;i--){
-        ans[i]=max(ans[i+1] , arr[i+1]);
-       }
-       return ans;
+    vector<int>lmax(vector<int>&arr){
+        int n=arr.size();
+        vector<int>ans(n);
+        ans[0]=arr[0];
+        
+        for(int i=1;i<n;i++){
+            ans[i]=max(arr[i],ans[i-1]);
+        }
+        return ans;
     }
     
-    int trap(vector<int>& height) {
-        int n=height.size();
-        int sum=0;
+    vector<int>rmax(vector<int>&arr){
+        int n=arr.size();
         vector<int>ans(n);
-
-        vector<int>left=leftMax(height);
-        vector<int>right=rightMax(height);
-
-        for(int i=0;i<n;i++){
-            if(height[i]>=left[i] || height[i]>=right[i])continue;
-            sum+=(min(left[i],right[i])-height[i]);
+        ans[n-1]=arr[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            ans[i]=max(arr[i],ans[i+1]);
         }
-        return sum;
+        return ans;
+    }
+
+
+
+    int trap(vector<int>& height) {
+        // 0 , n-1 -> 0 
+        //1 to n-2
+        // evry index -> ( min( rmax , lmax) - arr[i])
+
+        vector<int>rMax=rmax(height);
+        vector<int>lMax=lmax(height);
+
+        int sum=0;
+        for(int i=1;i<height.size()-1;i++){
+            sum+=(min(rMax[i],lMax[i])-height[i]);
+        }
+     return sum;
     }
 };
