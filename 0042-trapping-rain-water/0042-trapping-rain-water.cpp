bdb94ee240c7f7691151eea6ed416538423b514a
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-09-06 09:32:51 +0530

    Time: 0 ms (100%), Space: 26 MB (77.59%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-
-    vector<int>lmax(vector<int>&arr){
-        int n=arr.size();
-        vector<int>ans(n);
-        ans[0]=arr[0];
-        
-        for(int i=1;i<n;i++){
-            ans[i]=max(arr[i],ans[i-1]);
-        }
-        return ans;
-    }
-    
-    vector<int>rmax(vector<int>&arr){
-        int n=arr.size();
-        vector<int>ans(n);
-        ans[n-1]=arr[n-1];
-        
-        for(int i=n-2;i>=0;i--){
-            ans[i]=max(arr[i],ans[i+1]);
-        }
-        return ans;
-    }
-
-
-
     int trap(vector<int>& height) {
-        // 0 , n-1 -> 0 
-        //1 to n-2
-        // evry index -> ( min( rmax , lmax) - arr[i])
+        // water ? -> min(rmax , lmax)
+        // 2 pointers -> max water -> smaller of two -> move pointer with smaller ht
 
-        vector<int>rMax=rmax(height);
-        vector<int>lMax=lmax(height);
+        // lmax ,rmax -> if(lmax<rmax) -> lmax-arr[l] , l++;
+        // rmax-arr[r] , r--;
+        
+
+        int l=0;
+        int n=height.size();
+        int r = n-1;
+
+        int lmax=0;
+        int rmax=0;
 
         int sum=0;
-        for(int i=1;i<height.size()-1;i++){
-            sum+=(min(rMax[i],lMax[i])-height[i]);
+
+        while(l<r){
+            lmax=max(lmax,height[l]);
+            rmax=max(rmax, height[r]);
+
+            if(lmax<rmax){
+              sum+=(lmax-height[l]);
+              l++;
+            }
+            else{
+                sum+=(rmax-height[r]);
+                r--;
+            }
         }
-     return sum;
+        return sum;
     }
 };
