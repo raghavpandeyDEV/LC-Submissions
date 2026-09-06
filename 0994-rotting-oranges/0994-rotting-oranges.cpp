f283cb8080a66a36003b3bc4b25bc3d634393031
raghavpandeyDEV <241230040@nitdelhi.ac.in>
2026-09-06 18:07:10 +0530

    Time: 3 ms (25.54%), Space: 17.4 MB (10.65%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,47 +4,51 @@ public:
         int m=grid.size();
         int n=grid[0].size();
 
+        //{{r,c},t}
+
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(m , vector<int>(n,-1)); 
+
         int fresh=0;
-        vector<vector<int>>vis(m,vector<int>(n,-1));
-        queue<pair<pair<int,int>,int>>q; // {{r,c},t}
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1)fresh++;
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=1;
-                }
+              if(grid[i][j]==2){
+                q.push({{i,j},0});
+                vis[i][j]=1;
+              }
+              else if(grid[i][j]==1)fresh++;
             }
         }
         
+        vector<int>drow={-1,0,1,0}; 
+        vector<int>dcol={0,1,0,-1};
+
         int tm=0;
         int cnt=0;
+
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
+          int r=q.front().first.first;
+          int c=q.front().first.second;
+          int t=q.front().second;
+          q.pop();
 
-            tm=max(tm,t);
+          tm=max(tm,t);
 
-            q.pop();
-
-            vector<int> drow = {-1, 0, 1, 0};
-vector<int> dcol = {0, 1, 0, -1};
-
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
+          for(int i=0;i<4;i++){
+            int nrow=r+drow[i];
+            int ncol=c+dcol[i];
 if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && vis[nrow][ncol]==-1 && grid[nrow][ncol]==1){
     vis[nrow][ncol]=1;
-    q.push({{nrow,ncol},t+1});
+    q.push({{nrow,ncol}, t+1});
     cnt++;
 }
-            }
+          }
+
 
         }
-        if(cnt==fresh)return tm;
-        return -1;
-        
+if(cnt!=fresh)return -1;
+return tm;
+
     }
 };
